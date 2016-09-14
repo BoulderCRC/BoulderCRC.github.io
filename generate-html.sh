ddd8935c1b95337f70871ffe716549f0688fd041
Gregor Robinson <gregor@fiatflux.co.uk>
2016-09-13 18:29:25 -0600

    Add provost letter (draft)

--- generate-html.sh
+++ generate-html.sh
@@ -1,11 +1,14 @@
 #!/bin/env sh
 
+# First argument is page name; second argument is the header class.
 function generate_html {
     cat frontmatter.fragment.html >$1.html
     echo '      <body class="'$1'">' >>$1.html
+    cat header.fragment.html |sed s/%headerclass%/$2/g >>$1.html
     cat $1.fragment.html >>$1.html
     cat backmatter.fragment.html >>$1.html
     sed -i 's/\r//' $1.html
 }
 
-generate_html index
+generate_html index alt
+generate_html provost-letter-2016-09 ''
