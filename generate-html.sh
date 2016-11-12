d31ee5beecd3b8b75a227bc4f787ef260f4f4af9
Gregor Robinson <gregor@fiatflux.is>
2016-11-11 22:50:49 -0700

    Add wtf-is-a-regent.

--- generate-html.sh
+++ generate-html.sh
@@ -59,7 +59,8 @@ function generate_html {
 }
 
 # create "firstagrument.html" file with second argument as header class
-generate_html '' index alt
-generate_html '' provost-letter-2016-09 ''
-generate_html 'manual' index ''
-generate_html 'manual' rent ''
+generate_html '' 'index' 'alt'
+generate_html '' 'provost-letter-2016-09' ''
+generate_html 'manual' 'index' ''
+generate_html 'manual' 'rent' ''
+generate_html 'manual' 'wtf-is-a-regent' ''
