8b0662990e20a9be9b2db872313177d7279131f7
Gregor Robinson <garobinson@ucdavis.edu>
2019-05-13 13:16:37 -0600

    Add who, update header.

--- generate-html.sh
+++ generate-html.sh
@@ -63,6 +63,7 @@ function generate_html {
 generate_html '' 'index' 'alt'
 generate_html '' 'join' ''
 generate_html '' 'apply' ''
+generate_html '' 'who' ''
 generate_html '' 'provost-letter-2016-09' ''
 generate_html '' 'wages-petition-2017-10' ''
 generate_html '' 'fee-waiver-petition' ''
